84b2bbcabd1b45767ab3d4695f9cd43ee377157d
Pratik Khadka <kpratik@insightworkshop.io>
2020-09-05 18:20:55 +0545

    Updates

---
+++ precache-manifest.fa2162b74b31314e9d3343b3666909f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf82857b6e7c23dcd09d580bf622bcf",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "e14462c329abde3850e8",
+    "url": "/portfolio-react/static/css/2.8a097e4d.chunk.css"
+  },
+  {
+    "revision": "2cd76d0dba62446d47d4",
+    "url": "/portfolio-react/static/css/main.e4bc1eba.chunk.css"
+  },
+  {
+    "revision": "e14462c329abde3850e8",
+    "url": "/portfolio-react/static/js/2.314afa21.chunk.js"
+  },
+  {
+    "revision": "71245495fbffc19444723f9eab1bd702",
+    "url": "/portfolio-react/static/js/2.314afa21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd76d0dba62446d47d4",
+    "url": "/portfolio-react/static/js/main.d215a925.chunk.js"
+  },
+  {
+    "revision": "c345467e5c324ce14374",
+    "url": "/portfolio-react/static/js/runtime-main.7dd48b5e.js"
+  }
+]);
